7748a8821e5c13dbd7cfe76507857d0f86f9ae25
srinivasboyina <sri@tourtech.co.il>
2023-04-15 16:09:10 +0530

    UIViewController+Extension is added.

--- CommBankAssignmentSkeleton/Utilities/UIViewController + Extension.swift
+++ CommBankAssignmentSkeleton/Utilities/UIViewController + Extension.swift
@@ -5,4 +5,16 @@
 //  Created by apple on 15/04/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+
+    /**
+     *  Height of status bar + navigation bar (if navigation bar exist)
+     */
+
+    var topbarHeight: CGFloat {
+        return (view.window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0.0) +
+            (self.navigationController?.navigationBar.frame.height ?? 0.0)
+    }
+}
